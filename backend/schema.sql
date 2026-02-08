f033fa1a8fd9ad1fb054b12ed4dc44695cfb90d4
Oskar Wåglund <oskar.waglund@hotmail.com>
2026-02-08 01:35:31 +0100

    Set up some basic Wrangler

---
+++ backend/schema.sql
@@ -0,0 +1,14 @@
+-- Devices table
+CREATE TABLE IF NOT EXISTS Devices (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL UNIQUE
+);
+
+-- Readings table
+CREATE TABLE IF NOT EXISTS Readings (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  deviceId INTEGER NOT NULL,
+  temperature REAL NOT NULL,
+  timestamp TEXT NOT NULL,
+  FOREIGN KEY (deviceId) REFERENCES Devices(id)
+);
